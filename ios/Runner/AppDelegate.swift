eee03cf8a596fa5c6a3055bc89040b5a3309327f
bmartinho <bruno.m.martinho@gmail.com>
2025-07-09 10:14:18 +0100

    audio recordings not soft

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,6 +8,14 @@ import UIKit
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+    //Needed inorder to make audio recordings not soft
+      do {
+         let session = AVAudioSession.sharedInstance()
+         try session.setCategory(.playAndRecord, mode: .default, options: .defaultToSpeaker)
+         try session.setActive(true)
+      } catch {
+         debugPrint("Problem with AVAudioSession")
+      }
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
